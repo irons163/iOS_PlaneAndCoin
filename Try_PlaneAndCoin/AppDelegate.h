16f57b889d922d27cdcf48b2924e3e20228447a0
irons163 <irons@ironsdeMacBook-Air.local>
2016-04-13 19:14:57 +0800

    Initial Commit

---
+++ Try_PlaneAndCoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Try_PlaneAndCoin
+//
+//  Created by irons on 2016/4/13.
+//  Copyright (c) 2016年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Try_PlaneAndCoinTests/Try_PlaneAndCoinTests.m
@@ -0,0 +1,40 @@
+//
+//  Try_PlaneAndCoinTests.m
+//  Try_PlaneAndCoinTests
+//
+//  Created by irons on 2016/4/13.
+//  Copyright (c) 2016年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Try_PlaneAndCoinTests : XCTestCase
+
+@end
+
+@implementation Try_PlaneAndCoinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
